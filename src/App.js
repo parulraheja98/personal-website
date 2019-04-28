e174fad3c6be18790b19ef008fe08897b1280340
Parul Raheja <parulraheja98@gmail.com>
2019-04-28 15:45:23 -0400

    Styling Changes made

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import { Navbar, Nav, ListGroup } from "react-bootstrap";
+import SocialMediaIcons from 'react-social-media-icons';
 import "./App.css";
 
 class App extends Component {
   render() {
+    const socialMediaIcons = [
+      {
+        url: 'https://github.com/parulraheja98',
+        className: 'fa-github-square',
+      },
+      {
+        url: 'https://www.linkedin.com/in/parul-raheja-57566712a',
+        className: 'fa-linkedin',
+      },
+    ];
+
     return (
       <div>
         <Navbar bg="dark" variant="dark">
@@ -14,32 +26,32 @@ class App extends Component {
             <Nav.Link href="#contact">Contact </Nav.Link>
           </Nav>
         </Navbar>
-        <div style={{height:'20%'}}>
-          <img
-            src="./personalimg.jpg"
-            style={{
-              height: "300px",
-              width:"300px",
-              display:'block',
-              margin:'0px auto',
-              transform:"rotate(90deg)",
-              marginTop:'100px'
-            }}
-            id="headerImage"
-          />
+        <div className='main-header' style={{overflow:'hidden',backgroundColor:'antiquewhite'}}>
+          <div style={{height:'20%',marginTop:'5%'}}>
+            <img
+              src="./personalimg.jpeg"
+              style={{
+                height: "300px",
+                width:"300px",
+                display:'block',
+                margin:'0px auto',
+                borderRadius:'50%'
+              }}
+              id="headerImage"
+            />
+            </div>
+          <br />
+          <div id="header">
+            <h1 style={{ textAlign: "center" }}>
+              {" "}
+              Hi All, my name is Parul Raheja .
+            </h1>
           </div>
-        <br />
-        <div id="header">
-          <h1 style={{ textAlign: "center" }}>
-            {" "}
-            Hi All, my name is Parul Raheja .
-          </h1>
         </div>
-
         <div id="contentForInterest">
-          <ListGroup>
-            <ListGroup.Item variant="info">
-              <h1 id="projects"> Projects </h1>
+          <ListGroup className='navBar'>
+            <ListGroup.Item >
+              <h1 id="projects" className='headerContent'> Projects </h1>
             </ListGroup.Item>
 
             <ListGroup.Item>
@@ -47,76 +59,77 @@ class App extends Component {
                 {" "}
                 Developed a Common Chat Application for chatting among users{" "}
               </h3>
-              Technologies used :- HTML , CSS , JS , Laravel
+              <div>
+              Technologies used :- HTML , CSS , JS , Laravel, SQL <br />
+              <a href='https://github.com/parulraheja98/ChatProject'> Github URL </a>
+              </div>
               <br />
             </ListGroup.Item>
 
-            <ListGroup.Item>
-              <h3>
-                {" "}
-                Developed an Online Quiz Application for students in university{" "}
-              </h3>
-              Technologies Used :- HTML , CSS , ReactJS,Node JS
-            </ListGroup.Item>
 
             <ListGroup.Item>
               <h3>
                 {" "}
                 Developed an Online Shopping Cart {" "}
               </h3>
-              Technologies Used :- HTML , CSS , ReactJS,Node JS
+              <div>
+              Technologies Used :-  HTML , CSS , ReactJS,Express JS,MongoDB <br />
+              <a href='https://github.com/parulraheja98/shopping-cart-express-react'> Github URL </a>
+              </div>
             </ListGroup.Item>
 
             <ListGroup.Item>
-              <h3> Current Projects that i am working on include :- </h3>
-              Developing a household application for people living in a
-              household and sharing their expenses with each other . <br />
-              The application will help users in even distribution of expenses
-              among the users.
+              <h3> Developed a Household Expense Sharing App </h3>
+              <div>
+                Technologies Used :- HTML , CSS , ReactJS,Express JS,MongoDB <br />
+                <a href='https://github.com/parulraheja98/household-share-expense'> Github URL </a>
+              </div>
             </ListGroup.Item>
           </ListGroup>
 
-          <ListGroup>
-            <ListGroup.Item variant="info">
+          <ListGroup className='navBar'>
+            <ListGroup.Item >
               <div id="education">
-                <h1> Education </h1>
+                <h1 className='headerContent'> Education </h1>
                 <div id="university">
                   {" "}
-                  University of Winnipeg <span class="year">
+                  <h3><span className='uniTitle'>University of Winnipeg </span> <span class="year">
                     {" "}
-                    2016-2020{" "}
+                    
                   </span>{" "}
+                  </h3>
                 </div>
-                <div id="course"> BSC Applied Computer Science </div>
+                <div id="course"> BSC Applied Computer Science   &nbsp; <span> (2016-2020) </span> </div>
               </div>
             </ListGroup.Item>
 
             <ListGroup.Item
-              variant="info"
+             
               className="skills"
-              style={{ marginTop: 50 }}
+              style={{ }}
             >
               <div id="skills">
-                <h1> Skills </h1>
-                Programming Languages: Java, Node JS , C , C++ , PHP <br />
-                Web/Scripting: HTML, CSS, JavaScript,JQuery <br />
-                Web/Frameworks: Express.js, Meteor.js , Laravel <br />
-                Databases: MongoDB , SQL Server <br />
+                <h1 className='headerContent'> Skills </h1>
+                <h3>Programming Languages: </h3> 
+                <div>Java, Node JS , C , C++ , PHP <br /> </div>
+                <h3 className='skills-header'> Web/Scripting:</h3>
+                <div>  HTML, CSS, JavaScript,JQuery  </div>
+                <h3 className='skills-header'> Web/Frameworks:</h3>
+                <div> Express.js, Meteor.js , Laravel  </div>
+                <h3 className='skills-header'> Databases:</h3>
+                <div> MongoDB , SQL Server</div>
               </div>
             </ListGroup.Item>
 
-            <ListGroup.Item variant="info" style={{ marginTop: 50 }}>
+            <ListGroup.Item  style={{}}>
               <div id="contact">
-                <h2> Contact Information </h2>
-                Feel free to reach me on linked in at{" "}
-                <a href="https://www.linkedin.com/in/parul-raheja-57566712a">
-                  <img
-                    src="./linkedinContact.jpg"
-                    id="footerImage"
-                    style={{ height: "30px", width: "30px" }}
-                  />
-                </a>
-                <br />
+                <h2 className='headerContent'> Contact Information </h2>         
+                <SocialMediaIcons
+                  icons={socialMediaIcons}
+                  iconColor={'#495056'}
+                  className='socialMedia'
+                />
+                
                 Email :- parulraheja98@gmail.com
               </div>
             </ListGroup.Item>
