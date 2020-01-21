e32a9fea15cc5e132485ba9cf1654df58a0e9bf8
parulraheja98 <parulraheja98@gmail.com>
2020-01-20 23:37:58 -0600

    Formatting changes made

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -23,7 +23,7 @@ class Projects extends Component {
                         <div className="projContainer-expense">
                             <Card.Img className="imgEdit" variant="top" src="expenseSharing.PNG" />
                             <div class="overlay">
-                            <Button className="btnEdit" variant="light" onClick={() => this.props.handleClick("expenseshare")} >View Project</Button>
+                                <Button className="btnEdit" variant="light" onClick={() => this.props.handleClick("expenseshare")} >View Project</Button>
                             </div>
                         </div>
                     </StyledCard>
@@ -33,7 +33,7 @@ class Projects extends Component {
                         <div className="projContainer-salon">
                             <Card.Img variant="top" src="salonAppointment.PNG" />
                             <div class="overlay">
-                            <Button className="btnEdit" variant="light"  onClick={() => this.props.handleClick("salonappointment")}>View Project</Button>
+                                <Button className="btnEdit" variant="light" onClick={() => this.props.handleClick("salonappointment")}>View Project</Button>
                             </div>
                         </div>
                     </StyledCard>
@@ -41,9 +41,9 @@ class Projects extends Component {
                 <StyledDiv className="col-sm-6">
                     <StyledCard>
                         <div className="projContainer-chat">
-                            <Card.Img variant="top" width="100%" height="100%" src="chatApp.PNG"/>
+                            <Card.Img variant="top" width="100%" height="100%" src="chatApp.PNG" />
                             <div class="overlay">
-                            <Button className="btnEdit" variant="light"  onClick={() => this.props.handleClick("chatapp")} >View Project</Button>
+                                <Button className="btnEdit" variant="light" onClick={() => this.props.handleClick("chatapp")} >View Project</Button>
                             </div>
                         </div>
                     </StyledCard>
@@ -51,9 +51,9 @@ class Projects extends Component {
                 <StyledDiv className="col-sm-6">
                     <StyledCard>
                         <div className="projContainer-shopping">
-                            <Card.Img variant="top" width="100%" height="100%" src="shoppingCart.jpg"  />
+                            <Card.Img variant="top" width="100%" height="100%" src="shoppingCart.jpg" />
                             <div class="overlay">
-                            <Button className="btnEdit" variant="light" onClick={() => this.props.handleClick("shoppingcart")}>View Project</Button>
+                                <Button className="btnEdit" variant="light" onClick={() => this.props.handleClick("shoppingcart")}>View Project</Button>
                             </div>
                         </div>
                     </StyledCard>
